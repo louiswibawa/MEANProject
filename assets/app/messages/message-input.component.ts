eaca342d2c8aded819dd42526ef02933444c8775
Louis <louiswibawa@gmail.com>
2018-06-05 17:24:06 -0700

    Added ngModel

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
 	selector: 'app-message-input',
@@ -10,8 +11,10 @@ import { Message } from "./message.model";
 export class MessageInputComponent{
 	constructor(private messageService: MessageService) {}
 
-	onSave(value: string){
-		const message = new Message(value, 'Louis');
+	onSubmit(form: NgForm){
+		// console.log(form);
+		const message = new Message(form.value.content, 'Louis');
 		this.messageService.addMessage(message);
+		form.resetForm();
 	}
 }
