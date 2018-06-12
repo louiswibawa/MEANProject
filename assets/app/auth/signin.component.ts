8b7c9180568a931bc3810c170cfebf49e00ef60e
Louis <louiswibawa@gmail.com>
2018-06-12 16:57:50 -0700

    Finished the front-end. Worked on the sign in and deleteMessage

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
 	selector: 'app-signin',
@@ -7,5 +8,20 @@ import { Component } from "@angular/core";
 
 
 export class SigninComponent {
+	myForm: FormGroup;
 
+	onSubmit(){
+		console.log(this.myForm);
+		this.myForm.reset();
+	}
+
+	ngOnInit(){
+		this.myForm = new FormGroup({
+			email: new FormControl(null, [
+				Validators.required,
+				Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9]([a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)")
+			]),
+			password: new FormControl(null, Validators.required)
+		});
+	}
 }
